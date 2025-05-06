e7fba1adacb53c8e583e6db4b0680e27a6bbbc9a
msyakira <muthiasyakirah4@gmail.com>
2025-05-06 12:04:25 +0700

    Hapus project selain fasum

--- daftar_belanja/android/app/src/main/java/com/example/daftarr_belanjaa/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.daftarr_belanjaa;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- film/android/app/src/main/java/com/example/pilem/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.pilem;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
